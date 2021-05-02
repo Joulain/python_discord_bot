e21d25f044899f908739778ca6347d0a8e47eb8d
Joulain <vincent.joulain@viacesi.fr>
2021-05-02 13:44:19 +0000

    python3 corrected

--- reboot.sh
+++ reboot.sh
@@ -2,4 +2,4 @@
 
 ps ax | grep monitoring.py | awk 'NR==1{print $1}' | xargs kill
 
-nohup python -u ./monitoring.py &
+nohup python3 -u ./monitoring.py &
--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-nohup python -u ./monitoring &
+nohup python3 -u ./monitoring &
